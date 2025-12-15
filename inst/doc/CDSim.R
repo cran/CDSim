465098b0208b437c1ce7da226cbc4b9b5e34e2d1
Isaac Osei <ikemillar65@gmail.com>
2025-12-15 18:40:08 +0000

    version 0.1.1

---
+++ inst/doc/CDSim.R
@@ -0,0 +1,29 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  fig.width = 8,
+  fig.height = 4,
+  comment = "#>"
+)
+library(CDSim)
+
+## -----------------------------------------------------------------------------
+library(CDSim)
+
+## -----------------------------------------------------------------------------
+stations <- create_stations(n = 3, seed = 123)
+stations
+
+## -----------------------------------------------------------------------------
+sim <- simulate_climate_series(stations, start_year = 2019, end_year = 2024)
+head(sim)
+
+## -----------------------------------------------------------------------------
+write_station_csv(sim, file = "climate_data.csv")
+
+## -----------------------------------------------------------------------------
+write_station_netcdf(sim, out_nc = "climate_data.nc")
+
+## -----------------------------------------------------------------------------
+plot_station_timeseries(sim,'Station_1', var = "Avg.Tx")
+
